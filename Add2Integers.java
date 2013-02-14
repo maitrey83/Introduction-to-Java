362bef54116ed3347a996d46d9aa6f8d1656590a
S <s>
2013-02-14 12:52:37 -0500

    {"type":"TI", "runs":"1"}

--- Add2Integers.java
+++ Add2Integers.java
@@ -11,7 +11,7 @@ public class Add2Integers extends ConsoleProgram {
 
 	public void run() {
 		/* So all y'all in the back can see! */
-		setFont("DejaVuSerif-BOLD-10");
+		//setFont("DejaVuSerif-BOLD-10");
 
 		println("This program adds two numbers.");
 		int n1 = readInt("Enter n1: ");
