01d0d959237dfaf7bc56365af6397e2159cfc188
S <s>
2013-02-14 20:23:21 -0500

    {"type":"TI", "runs":"3"}

--- Add2Integers.java
+++ Add2Integers.java
@@ -9,20 +9,21 @@ import acm.program.*;
 
 public class Add2Integers extends ConsoleProgram {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public void run() {
 		/* So all y'all in the back can see! */
 		setFont("DejaVuSerif-BOLD-10");
 
-		/*println("This program adds two numbers.");
+		println("This program adds two numbers.");
 		int n1 = readInt("Enter n1: ");
 		int n2 = readInt("Enter n2: ");
 		int total = n1 + n2;
-		println("The total is " + total + ".");*/
+		println("The total is " + total + ".");
 		
-		println("This program converts fahrenheit to celsius");
-		//int fahrenheit = 45;
-		int celsius = (45 - 32) * (5 / 9);
-		println("Celsius is - " + celsius + " degree");
 	}
 
 }
